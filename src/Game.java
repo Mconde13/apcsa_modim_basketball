8a1707769ed380d2e350b2196ac9029a4de306db
Mohamed <mmconde13@gmail.com>
2019-06-07 08:40:05 -0400

    mo edits

--- src/Game.java
+++ src/Game.java
@@ -1,13 +1,13 @@
 public class Game {
-
   private Grid grid;
   private int userRow;
   private int msElapsed;
   private int timesGet;
   private int timesAvoid;
+  private String defPic = "avoid.png";
+  private String hoopPic =  "get.png";
   
   public Game() {
-
     grid = new Grid(5, 10);
     userRow = 0;
     msElapsed = 0;
@@ -18,7 +18,6 @@ public class Game {
   }
   
   public void play() {
-
     while (!isGameOver()) {
       grid.pause(100);
       handleKeyPress();
@@ -34,41 +33,40 @@ public class Game {
   public void handleKeyPress(){
     int key = grid.checkLastKeyPressed();
     System.out.println(key);
-
     if((key == 87 || key == 38) && userRow != 0){
       userRow--;
-
       Location loc = new Location(userRow,0);
       grid.setImage(loc,"user.gif");
-
       Location oldLoc = new Location(userRow+1,0);
       grid.setImage(oldLoc,null);
-
    }else if((key == 40 || key == 83) && userRow != 4){
-
     userRow++;
-
     Location loc = new Location(userRow,0);
     grid.setImage(loc,"user.gif");
     
     Location oldLoc = new Location(userRow-1,0);
     grid.setImage(oldLoc,null);
-
 }
   
 }
-
   
-  public void populateRightEdge(){
-
+private void populateRightEdge() {
+for(int i = 0; i < grid.getNumRows();i++){
+  Location loc = new Location(i,grid.getNumCols()-1);
+  double random = Math.random();
+  if (random < .1) {
+    grid.setImage(loc, defPic);
   }
+  else if (random < .3) {
+    grid.setImage(loc,hoopPic);
+   }
+  }
+}
   
   public void scrollLeft(){
-
   }
   
   public void handleCollision(Location loc) {
-
   }
   
   public int getScore() {
@@ -80,7 +78,11 @@ public class Game {
   }
   
   public boolean isGameOver() {
-    return false;
+    if (getScore()<-1){
+			System.out.println("You are dum wack");
+			return true;
+		}
+		return false;
   }
     
   public static void main(String[] args) {
