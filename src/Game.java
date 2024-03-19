8bc36e739e69d3f39ad9472505b1cae8f6c0d46c
khadim24 <50873881+khadim24@users.noreply.github.com>
2024-03-19 19:49:18 -0400

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -6,15 +6,17 @@ public class Game {
   private int timesGet;
   private int timesAvoid;
   private String defPic = "imageedit_3_5974758706.png";
-  private String ballPic =  "ball.png";
+  private String ballPic = "ball.png";
   private String userPic = "58888515bc2fc2ef3a186097.png";
   private int counter = 0;
   private String backGround = "animated-cartoon-blue-sky-with-white-clouds_vj92cw5je__F0000.png";
   private String swish = "src/swoosh.wav";
   private String error = "src/banana.wav";
-  private WavPlayer song = new WavPlayer("y2mate.com - kurtis_blow_basketball__shxzlTRK44.wav");
+  private String backgroundSong = "basketball.wav";
+  private WavPlayer song = new WavPlayer(backgroundSong);
+
   public Game() {
-    grid = new Grid(15, 28,backGround);
+    grid = new Grid(15, 28, backGround);
     userRow = 14;
     userCol = 27;
     msElapsed = 0;
@@ -23,16 +25,15 @@ public class Game {
     updateTitle();
     grid.setImage(new Location(userRow, userCol), userPic);
   }
-  
+
   public void play() {
     while (!isGameOver()) {
       grid.pause(100);
-      
+
       handleKeyPress();
       if (msElapsed % 300 == 0) {
-        
+
         dropThings();
-        touchesBottom();
         handleCollision();
         refreshHoop();
         populateNewThings();
@@ -40,137 +41,123 @@ public class Game {
       updateTitle();
       msElapsed += 100;
     }
+
   }
-  
-  public void handleKeyPress(){
+
+  public void handleKeyPress() {
     int key = grid.checkLastKeyPressed();
     System.out.println(key);
 
-
-    if((key == 87 || key == 38) && userRow != 0){
+    if ((key == 87 || key == 38) && userRow != 0) {
       userRow--;
-      Location loc = new Location(userRow,userCol);
-      grid.setImage(loc,userPic);
-      Location oldLoc = new Location(userRow+1,userCol);
-      grid.setImage(oldLoc,null);
-   
-   
-    }else if((key == 40 || key == 83) && userRow != grid.getNumRows()-1){
-    userRow++;
-    Location loc = new Location(userRow,userCol);
-    grid.setImage(loc,userPic);
-    
-    Location oldLoc = new Location(userRow-1,userCol);
-    grid.setImage(oldLoc,null);
-}
-  
+      Location loc = new Location(userRow, userCol);
+      grid.setImage(loc, userPic);
+      Location oldLoc = new Location(userRow + 1, userCol);
+      grid.setImage(oldLoc, null);
 
-//left right keys
-else if((key == 37 || key == 65) && userCol != 0){
-  userCol--;
-  Location loc = new Location(userRow,userCol);
-  grid.setImage(loc,userPic);
-  Location oldLoc = new Location(userRow,userCol+1);
-  grid.setImage(oldLoc,null);
+    } else if ((key == 40 || key == 83) && userRow != grid.getNumRows() - 1) {
+      userRow++;
+      Location loc = new Location(userRow, userCol);
+      grid.setImage(loc, userPic);
 
-  
-}else if((key == 39 || key == 68) && userCol != grid.getNumCols() - 1){
-userCol++;
-Location loc = new Location(userRow,userCol);
-grid.setImage(loc,userPic);
+      Location oldLoc = new Location(userRow - 1, userCol);
+      grid.setImage(oldLoc, null);
+    }
 
-Location oldLoc = new Location(userRow,userCol-1);
-grid.setImage(oldLoc,null);
-}
+    // left right keys
+    else if ((key == 37 || key == 65) && userCol != 0) {
+      userCol--;
+      Location loc = new Location(userRow, userCol);
+      grid.setImage(loc, userPic);
+      Location oldLoc = new Location(userRow, userCol + 1);
+      grid.setImage(oldLoc, null);
 
+    } else if ((key == 39 || key == 68) && userCol != grid.getNumCols() - 1) {
+      userCol++;
+      Location loc = new Location(userRow, userCol);
+      grid.setImage(loc, userPic);
 
-}
-  
-private void populateNewThings() {
-for(int i = 0; i < grid.getNumCols();i++){
-  Location loc = new Location(0,i);
-  double random = Math.random();
-  if (random < .025) {
-    grid.setImage(loc, defPic);
+      Location oldLoc = new Location(userRow, userCol - 1);
+      grid.setImage(oldLoc, null);
+    }
+
   }
-  else if (random < .05) {
-    grid.setImage(loc,ballPic);
-   }
-  }
-}
-  
-public void dropThings(){
-for(int i = grid.getNumRows()-1; i >= 0 ;i--){
-  for(int j = 0; j < grid.getNumCols();j++){
-Location oldLoc = new Location(i, j);
-Location newLoc = new Location(i+1,j);
 
-if(i==14){
-grid.setImage(oldLoc,null); 
-
-} else if(ballPic.equals(grid.getImage(oldLoc))){
-
-grid.setImage(oldLoc, null);
-  grid.setImage(newLoc, ballPic);
-} else if(defPic.equals(grid.getImage(oldLoc))){
-  grid.setImage(oldLoc, null);
-  grid.setImage(newLoc, defPic);
-}
-  }
-}
-  }
-  public void touchesBottom() {
-    for(int i = 0; i < grid.getNumCols();i++){
-    String itemAtCell = grid.getImage(new Location(14,i));
-    if((ballPic).equals(itemAtCell)){
-      WavPlayer.play(error);
-      counter--;
+  private void populateNewThings() {
+    for (int i = 0; i < grid.getNumCols(); i++) {
+      Location loc = new Location(0, i);
+      double random = Math.random();
+      if (random < .025) {
+        grid.setImage(loc, defPic);
+      } else if (random < .05) {
+        grid.setImage(loc, ballPic);
+      }
     }
   }
-  
-}
-  public void handleCollision() {
-    String itemAtCell = grid.getImage(new Location(userRow,userCol));
- if(itemAtCell == null|| itemAtCell.equals(userPic)){
-   return;
- }
- if(itemAtCell.equals(ballPic)){
-   WavPlayer.play(swish);
- counter = counter + 2;
- }else{
-   WavPlayer.play(error);
-   counter = counter - 1;
- }
- 
- }
-  
 
-  public void refreshHoop(){
-    grid.setImage(new Location(userRow,userCol),userPic);
+  public void dropThings() {
+    for (int i = grid.getNumRows() - 1; i >= 0; i--) {
+      for (int j = 0; j < grid.getNumCols(); j++) {
+        Location oldLoc = new Location(i, j);
+        Location newLoc = new Location(i + 1, j);
+
+        if (i == 14) {
+          grid.setImage(oldLoc, null);
+
+        } else if (ballPic.equals(grid.getImage(oldLoc))) {
+
+          grid.setImage(oldLoc, null);
+          grid.setImage(newLoc, ballPic);
+        } else if (defPic.equals(grid.getImage(oldLoc))) {
+          grid.setImage(oldLoc, null);
+          grid.setImage(newLoc, defPic);
+        }
+      }
+    }
   }
-  
+
+  public void handleCollision() {
+    String itemAtCell = grid.getImage(new Location(userRow, userCol));
+    if (itemAtCell == null || itemAtCell.equals(userPic)) {
+      return;
+    }
+    if (itemAtCell.equals(ballPic)) {
+      WavPlayer.play(swish);
+      counter = counter + 3;
+    } else {
+      WavPlayer.play(error);
+      counter = counter - 5;
+    }
+
+  }
+
+  public void refreshHoop() {
+    grid.setImage(new Location(userRow, userCol), userPic);
+  }
+
   public int getScore() {
     return counter;
   }
-  
+
   public void updateTitle() {
     grid.setTitle("Score:  " + getScore());
   }
-  
+
   public boolean isGameOver() {
-    if (getScore()<=-30){
+    if (getScore() <= -30) {
       System.out.println("You are bad, try again");
-      if(getScore()>=30){
-        System.out.println("You win");
-      }
-			return true;
-		}
-		return false;
+      return true;
+    } else if (getScore() >= 30) {
+      System.out.println("You win");
+      return true;
+    }
+    return false;
   }
-    
+
   public static void main(String[] args) {
-    Game game = new Game();  
+    Game game = new Game();
+    game.song.startSound(); // Play the background song
     game.play();
-    //new code here
+    // new code here
   }
 }
